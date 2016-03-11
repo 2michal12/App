6b0d598ae973514f1977bf20c3f9c853c5165f61
Michal Majzel <michal.majzel@erstegroupit.com>
2016-03-11 12:15:48 +0100

    algorithm for finding fragments

--- app/src/main/java/com/example/michal/myapplication/MaxMin.java
+++ app/src/main/java/com/example/michal/myapplication/MaxMin.java
@@ -9,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.opencv.android.Utils;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
@@ -22,6 +23,7 @@ import butterknife.ButterKnife;
  */
 public class MaxMin extends AppCompatActivity {
     Bitmap imageBitmap = null, imageAfter;
+    Mat image,color_image;
 
     @Bind(R.id.text)
     TextView text;
@@ -48,7 +50,8 @@ public class MaxMin extends AppCompatActivity {
         imageView.setImageBitmap(imageBitmap);
 
         Help help = new Help(this);
-        Mat image = help.bitmap2mat(imageBitmap);
+        image = help.bitmap2mat(imageBitmap);
+        color_image = new Mat(image.rows(), image.cols(), CvType.CV_8UC3);
         Imgproc.cvtColor(image, image, Imgproc.COLOR_RGB2GRAY);
 
         if( !convertSkeleton(image) ){
@@ -71,7 +74,7 @@ public class MaxMin extends AppCompatActivity {
 
         for(int i = 0; i < image.height(); i++){
             for(int j = 0; j < image.width(); j++){
-                if(image.get(i, j)[0] == 255){
+                if(image.get(i, j)[0] > 100){
                     image.put(i, j, black);
                 }else{
                     image.put(i, j, white);
@@ -84,9 +87,11 @@ public class MaxMin extends AppCompatActivity {
 
     public void extraction(Mat image, int type){
         int BLOCK_SIZE = 7;
+
         double[] data = new double[1];
         data[0] = 0;
-        int cn;
+        int progress = 0;
+        int cn = 0;
         int[][] endings = new int[2][image.rows()*image.cols()];
         int[][] bifurcation = new int[2][image.rows()*image.cols()];
         int countBifurcation = 0;
@@ -126,41 +131,191 @@ public class MaxMin extends AppCompatActivity {
 
                         }
                     }
+
             }
         }
 
-        int fix_val_x, fix_val_y;
-        int x, y;
-        int SIZE = 10;
-        for(int j = 0; j < countEndings; j++) {
-            fix_val_x = endings[1][j];
-            fix_val_y = endings[0][j];
 
-            for(int i = 0; i < countEndings; i++) {
-                x = Math.abs(fix_val_x - endings[1][i]);
-                y = Math.abs(fix_val_y - endings[0][i]);
-                if( (endings[0][i]!=fix_val_y) && (endings[1][i]!=fix_val_x) && (x <= SIZE) && (y <= SIZE)  ){
-                    endings[1][i] = 0;
-                    endings[0][i] = 0;
-                    endings[1][j] = 0;
-                    endings[0][j] = 0;
-                    break;
+        //copy image to color image
+        double[] black = new double[3];
+        black[0] = 0;
+        black[1] = 0;
+        black[2] = 0;
+        double[] white = new double[3];
+        white[0] = 255;
+        white[1] = 255;
+        white[2] = 255;
+
+        for(int i = 0; i < image.height(); i++){
+            for(int j = 0; j < image.width(); j++){
+                if(image.get(i, j)[0] > 100){
+                    color_image.put(i, j, white);
+                }else{
+                    color_image.put(i, j, black);
+                }
+
+            }
+        }
+
+        int dlzka_fragmentu = 2;
+        int[][] fragment = new int[2][dlzka_fragmentu];
+
+        double[] white_pixel;
+        double[] point = new double[2];
+        double[] next_point = new double[2];
+        next_point[0] = 0;
+        next_point[1] = 0;
+        boolean erase = false;
+        int count;
+        double[] black1 = new double[1];
+        black1[0] = 0;
+
+        for(int i = 0; i < image.height(); i++) {
+            for (int j = 0; j < image.width(); j++) {
+                white_pixel = image.get(i,j);
+                if(white_pixel[0] == 255){
+                    point[0] = i;
+                    point[1] = j;
+                    count = 1;
+                    for(int k = 1; k < dlzka_fragmentu; k++) { //dlzka fragmentu
+                        erase = false;
+                        next_point = testNeighbour(point);
+                        fragment[0][0] = (int)point[0];
+                        fragment[1][0] = (int)point[1];
+                        if (next_point[0] == -1 && next_point[1] == -1) {
+                            count++;
+                            erase = true;
+                            break;
+                        } else {
+                            count++;
+                            point[0] = next_point[0];
+                            point[1] = next_point[1];
+                            fragment[0][k] = (int)next_point[0];
+                            fragment[1][k] = (int)next_point[1];
+                            if(k == dlzka_fragmentu-1)
+                                erase = true;
+                            else
+                                erase = false;
+                        }
+                    }
+                    if(erase){
+                        //vymazat fragment
+                        for (int k = 0; k < count; k++){
+                            image.put(fragment[0][k],fragment[1][k], black1);
+                        }
+                        erase = false;
+                    }
                 }
             }
         }
 
-            if( type == 1 ){
-            for(int i = 0; i < countEndings; i++){
-                Point core = new Point(endings[1][i], endings[0][i]);
-                Imgproc.circle(image, core, 8, new Scalar(150, 0, 0), 2);
+
+        /*
+        //print minutie
+        int SIZE = 15;
+        int fix_val_x, fix_val_y;
+
+        if(type==1){
+
+            //cistenie blizkych markantov
+            for(int j = 0; j < countEndings; j++) {
+                fix_val_x = endings[1][j];
+                fix_val_y = endings[0][j];
+
+                for(int i = 0; i < countEndings; i++) {
+                    if( (endings[0][i]!=fix_val_y) && (endings[1][i]!=fix_val_x) && ( ( Math.abs(fix_val_x - endings[1][i])) <= SIZE) && ( (Math.abs(fix_val_y - endings[0][i])) <= SIZE)  ){
+                        endings[1][i] = 0;
+                        endings[0][i] = 0;
+                        endings[1][j] = 0;
+                        endings[0][j] = 0;
+                        break;
+                    }
+                }
             }
-        }else if( type == 2 ){
+
+            for(int i = 0; i < countEndings; i++){
+                if(endings[1][i] != 0 && endings[0][i] != 0) {
+                    Point core = new Point(endings[1][i], endings[0][i]);
+                    Imgproc.circle(color_image, core, 8, new Scalar(251, 18, 34), 2);
+                }
+            }
+        }else if(type ==2){
+
+            //cistenie blizkych markantov
+            for(int j = 0; j < countBifurcation; j++) {
+                fix_val_x = bifurcation[1][j];
+                fix_val_y = bifurcation[0][j];
+
+                for(int i = 0; i < countBifurcation; i++) {
+                    if( (bifurcation[0][i]!=fix_val_y) && (bifurcation[1][i]!=fix_val_x) && ( ( Math.abs(fix_val_x - bifurcation[1][i])) <= SIZE) && ( (Math.abs(fix_val_y - bifurcation[0][i])) <= SIZE)  ){
+                        bifurcation[1][i] = 0;
+                        bifurcation[0][i] = 0;
+                        bifurcation[1][j] = 0;
+                        bifurcation[0][j] = 0;
+                        break;
+                    }
+                }
+            }
+
             for(int i = 0; i < countBifurcation; i++){
-                Point core = new Point(bifurcation[1][i], bifurcation[0][i]);
-                Imgproc.circle(image, core, 8, new Scalar(150, 0, 0), 2);
+                if(bifurcation[1][i] != 0 && bifurcation[0][i] != 0) {
+                    Point core = new Point(bifurcation[1][i], bifurcation[0][i]);
+                    Imgproc.circle(color_image, core, 8, new Scalar(102, 255, 51), 2);
+                }
             }
         }
+        */
+    }
 
+    double[] testNeighbour(double[] prev){
+        int count = 0;
+        double[] new_point = new double[2];
+        double row_d = prev[0];
+        double col_d = prev[1];
+        int row = (int)row_d;
+        int col = (int)col_d;
+
+        if(image.get(row-1,col-1)[0] == 255 && (prev[0]!=row-1) && (prev[1]!=col-1) ){
+            count++;
+            new_point[0]=row-1;
+            new_point[1]=col-1;
+        }else if(image.get(row-1,col)[0] == 255 && (prev[0]!=row-1) && (prev[1]!=col) ){
+            count++;
+            new_point[0]=row-1;
+            new_point[1]=col;
+        }else if(image.get(row-1,col+1)[0] == 255 && (prev[0]!=row-1) && (prev[1]!=col+1) ){
+            count++;
+            new_point[0]=row-1;
+            new_point[1]=col+1;
+        }else if(image.get(row,col-1)[0] == 255 && (prev[0]!=row) && (prev[1]!=col-1) ){
+            count++;
+            new_point[0]=row;
+            new_point[1]=col-1;
+        }else if(image.get(row,col+1)[0] == 255 && (prev[0]!=row) && (prev[1]!=col+1) ){
+            count++;
+            new_point[0]=row;
+            new_point[1]=col+1;
+        }else if(image.get(row+1,col-1)[0] == 255 && (prev[0]!=row+1) && (prev[1]!=col-1) ){
+            count++;
+            new_point[0]=row+1;
+            new_point[1]=col-1;
+        }else if(image.get(row+1,col)[0] == 255 && (prev[0]!=row+1) && (prev[1]!=col) ){
+            count++;
+            new_point[0]=row+1;
+            new_point[1]=col;
+        }else if(image.get(row+1,col+1)[0] == 255 && (prev[0]!=row+1) && (prev[1]!=col+1) ){
+            count++;
+            new_point[0]=row+1;
+            new_point[1]=col+11;
+        }
+
+        if(count == 1){
+            return new_point;
+        }else{
+            new_point[0]=-1;
+            new_point[1]=-1;
+            return new_point;
+        }
     }
 
 
